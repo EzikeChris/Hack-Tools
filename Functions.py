232adc13a889ed53f061cbd33bcccf701c824360
chrisezike <chrisezike@kali.chris>
2020-11-19 10:06:07 +0100

    initial commit

---
+++ Functions.py
@@ -0,0 +1,17 @@
+def who_am_i():
+    name = "Health"
+    age = 27
+    print("My Name is " + name + " and I am " + str(age) + "years old.")
+who_am_i()
+
+
+
+#adding parameters
+
+def add_five_hundred(num):
+    print(num + 500)
+add_five_hundred(500)
+
+def add(y,x):
+    print(y + x)
+add(7,7)
---
+++ meta.py
@@ -0,0 +1,2 @@
+print('hello world')
+print('\n') #RUN NEW LINE  ON THE TERMINAL
